a2a5ba47e74856a180f17f077ee1a1850c6718f8
Gaurishankar Khadga <ggs699000@gmail.com>
2026-03-17 10:19:04 +0530

    gshankar added sienthing

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+
+
+
 const app = express();
+
+
 
 // ==================== CORS ====================
 const allowedOrigins = [...new Set([
